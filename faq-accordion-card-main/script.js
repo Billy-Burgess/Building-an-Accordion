f2e3d0006412d512305473fa37dda7f1e93218ef
B_Burgess <140908055+Billy-Burgess@users.noreply.github.com>
2023-12-08 17:18:38 +0000

    Coding the JS
    Following the walkthrough tutorial and finished here @ 35:50 on the part 2 video on YT.

--- faq-accordion-card-main/script.js
+++ faq-accordion-card-main/script.js
@@ -1,1 +1,7 @@
-console.log('hello');
+let accordionButton = document.querySelectorAll('.accordion__question').forEach(item => {
+    item.addEventListener('click', event => {
+        console.log('click');
+        item.classList.toggle('open');
+        item.nextElementSibling.classList.toggle('open');
+    })
+})
